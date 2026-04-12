e976f480127d734ab89b5f3b30cbbe881f91be7a
IlberYek <ilberyaylacioglu0@gmail.com>
2026-04-12 16:51:24 +0300

    Add solution for Project Euler Problem 1
    Implement solution for Project Euler Problem 1 to find the sum of multiples of 3 or 5 below 1000.

---
+++ Project-Euler/Problem_01.py
@@ -0,0 +1,13 @@
+# Project Euler - 1
+# Question: 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+total = 0 # That's our piggy bank. We'll just add on to this as we find 'i's.
+for i in range(1000):
+    if i % 3 == 0 or i % 5 == 0:
+        total += i
+        print(total)
+    else:
+        continue
+print(total)
